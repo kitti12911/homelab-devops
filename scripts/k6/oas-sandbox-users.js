f08a49694a9a8877ef0eeace87af85040e38567e
Kitti Srijantanakul <kitti12911@gmail.com>
2026-05-10 03:11:40 +0700

    chore: config test

--- scripts/k6/oas-sandbox-users.js
+++ scripts/k6/oas-sandbox-users.js
@@ -3,27 +3,28 @@ import { check, sleep } from "k6";
 
 export const options = {
     stages: [
-        { duration: "10s", target: 10000 },
-        { duration: "1m", target: 10000 },
-        { duration: "10s", target: 0 },
-    ],
+        { duration: "10s", target: 5000 },
+        { duration: "1m", target: 5000 },
+        { duration: "10s", target: 0 }
+    ]
 };
 
 export default function () {
     const response = http.get("https://oas-sandbox.lan/health", {
         headers: {
             accept: "application/json",
+            "accept-encoding": "gzip"
         },
         tags: {
             api: "GET /health",
-            service: "oas-sandbox",
-        },
+            service: "oas-sandbox"
+        }
     });
 
     check(response, {
         "status is 200": (r) => r.status === 200,
         "content-type is json": (r) =>
-            (r.headers["Content-Type"] || "").includes("application/json"),
+            (r.headers["Content-Type"] || "").includes("application/json")
     });
 
     sleep(1);
